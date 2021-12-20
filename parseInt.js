002101fd1afcbde4191c636978f2bb6ac22848b3
Khaair <khair.cse.iu@gmail.com>
2021-12-20 14:12:48 +0600

    update

--- parseInt.js
+++ parseInt.js
@@ -31,3 +31,10 @@ const someAdjective = "awesome";
 let myStr = "Learning to code is ";
 
 myStr += someAdjective
+
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;

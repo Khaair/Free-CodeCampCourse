09945b3baab44312c9952020bb972a20e20a856d
Khaair <khair.cse.iu@gmail.com>
2021-12-20 10:10:23 +0600

    update

--- parseInt.js
+++ parseInt.js
@@ -13,3 +13,5 @@ function convertToInteger(str) {
        }
        
        console.log(convertToInteger("10011"))
+
+       const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line

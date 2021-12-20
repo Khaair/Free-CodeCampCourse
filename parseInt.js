683e89d784bf1a83650adf087770d28341456d7a
Khaair <khair.cse.iu@gmail.com>
2021-12-20 09:54:28 +0600

    update

--- parseInt.js
+++ parseInt.js
@@ -6,3 +6,10 @@ function convertToInteger(str) {
     convertToInteger("56");
     convertToInteger("77");
     convertToInteger("JamesBond");
+
+    function convertToInteger(str) {
+        const a = parseInt(str, 2);
+        return a
+       }
+       
+       console.log(convertToInteger("10011"))

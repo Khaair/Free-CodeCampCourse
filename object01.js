a62ab7ad1925b96dca46d1f9042fbd6e08ee8f50
Khaair <khair.cse.iu@gmail.com>
2021-12-21 22:47:59 +0600

    update

---
+++ object01.js
@@ -0,0 +1,13 @@
+const myDog = {
+    // Only change code below this line
+  
+    "name" :  "Whiskers",
+    "legs" : 5,
+    "tails" : 8,
+    "friends" : ["Water", "Dogs"]
+  
+  
+  
+  
+    // Only change code above this line
+  };

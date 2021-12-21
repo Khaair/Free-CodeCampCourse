2df11e3df1ed09c0ac786d8b22a79e52be9d5ee9
Khaair <khair.cse.iu@gmail.com>
2021-12-21 16:10:49 +0600

    update

--- equalityoperator.js
+++ equalityoperator.js
@@ -38,3 +38,15 @@ function testNotEqual(val) {
   }
   
   testNotEqual(99);
+
+
+
+  // Setup
+function testStrictNotEqual(val) {
+    if (val !== 17) { // Change this line
+      return "Not Equal";
+    }
+    return "Equal";
+  }
+  
+  testStrictNotEqual(17);

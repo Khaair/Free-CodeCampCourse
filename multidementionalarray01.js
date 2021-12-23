170c120db494333e32bbe7db5eb5254f6e6fdbe1
Khaair <khair.cse.iu@gmail.com>
2021-12-23 15:40:08 +0600

    update

--- multidementionalarray01.js
+++ multidementionalarray01.js
@@ -7,3 +7,14 @@ function countdown(n){
     return n < 1 ? [] : [n].concat(countdown(n - 1));
  }
  // Only change code above this line
+
+
+ function rangeOfNumbers(startNum, endNum) {
+    if (endNum - startNum === 0) {
+      return [startNum];
+    } else {
+      var numbers = rangeOfNumbers(startNum, endNum - 1);
+      numbers.push(endNum);
+      return numbers;
+    }
+  }

8f5ffb4558bc59cb0e6d74cb2d31d5117303ae7d
Khaair <khair.cse.iu@gmail.com>
2021-12-23 15:38:27 +0600

    update

--- multidementionalarray01.js
+++ multidementionalarray01.js
@@ -1,1 +1,9 @@
 const myList = [["Chocolate Bar", 15],["Chocolate Bar", 15],["Chocolate Bar", 15],["Chocolate Bar", 15],["Chocolate Bar", 15]];
+
+
+// Only change code below this line
+
+function countdown(n){
+    return n < 1 ? [] : [n].concat(countdown(n - 1));
+ }
+ // Only change code above this line
